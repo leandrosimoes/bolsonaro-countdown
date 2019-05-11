f25c87c46a7da54d0cbb4b8c47f8379c5032a8f7
Leandro Simões <leandro.simoes@outlook.com>
2019-05-11 10:44:05 -0300

    finished the first version

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+(() => {
+    document.addEventListener("DOMContentLoaded", function () {
+        let targetDate = new Date(2022, 0, 1);
+        let onStart = () => {
+            document.querySelectorAll('.countdown__item').forEach(item => item.classList.add('show'))
+        }
+        let onTick = ({ years, days, hours, minutes, seconds }) => {
+            document.querySelector('.countdown__item.years').innerHTML = years;
+            document.querySelector('.countdown__item.days').innerHTML = days;
+            document.querySelector('.countdown__item.hours').innerHTML = hours;
+            document.querySelector('.countdown__item.minutes').innerHTML = minutes;
+            document.querySelector('.countdown__item.seconds').innerHTML = seconds;
+        };
+        let sufixes = new LsCountdownSufixes({ years: 'a' })
+        let options = new LsCountdownOptions({ targetDate, onStart, onTick, sufixes });
+        let countdown = new LsCountdown(options);
+
+        countdown.start();
+        window.mycountdown = countdown;
+    });
+})();
